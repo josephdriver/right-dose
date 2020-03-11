43378a8a0eb6a5ce46b856ed78273b5b282a89a4
Joseph Driver <joseph.driver0@gmail.com>
2020-03-11 20:44:25 +0800

    fixed delete function

--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -2,7 +2,7 @@ class Presentation < ApplicationRecord
   DOSE_UNITS = ["", "g", "mg", "mcg"]
   VOLUME_UNITS = ["ml", "EpiPen"]
   belongs_to :drug
-  has_many :indications
+  has_many :indications, dependent: :destroy
   validates :dose_unit, inclusion: { in: DOSE_UNITS}
   validates :volume_unit, inclusion: { in: VOLUME_UNITS}
 end
