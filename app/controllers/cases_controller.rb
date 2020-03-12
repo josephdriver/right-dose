1be6977e6f87c20b6fa467024a7d3b2bb2e2aea6
Rich Cherry <rich.m.cherry@gmail.com>
2020-03-12 12:33:13 +0800

    modal works

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,12 +1,13 @@
 class CasesController < ApplicationController
   def new
-    @user = current_user
     @case = Case.new
+    authorize @case
   end
 
   def create
     @case = Case.new(case_params)
     @case.user = current_user
+    raise
     if @case.save
       # redirect_to paramedic_dashboard_path(@case)
     else
