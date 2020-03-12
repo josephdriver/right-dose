bc00b3a3d9bbc0f7eda0a3b9146769dcf0b263aa
Joseph Driver <joseph.driver0@gmail.com>
2020-03-12 10:34:24 +0800

    completed drug destroy functionality

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -1,13 +1,31 @@
 class PresentationsController < ApplicationController
+  def index
+    @drugs = policy_scope(Drug)
+    @admin = current_admin
+  end
+
   def new
+    @prensentation = Presentation.new
+    authorize @prensentation
   end
 
   def create
+    @presentation = Presentation.new(presentation_params)
+    authorize @presentation
+    if @presentation.save
+      redirect_to drugs_path
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def presentation_params
+    params.require(:presentation).permit(:drug_id, :dose, :dose_unit, :volume, :volume_unit)
   end
 end
