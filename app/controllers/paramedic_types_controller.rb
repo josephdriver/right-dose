4faad3ffbc31f0b598233ba836cc3e551869031c
Yassin Elsir <yassinmelsir@gmail.com>
2020-03-13 13:05:30 +0800

    created edit forms

--- app/controllers/paramedic_types_controller.rb
+++ app/controllers/paramedic_types_controller.rb
@@ -9,6 +9,23 @@ class ParamedicTypesController < ApplicationController
   def create
   end
 
+  def edit
+    @paramedic_type = ParamedicType.find(params[:id])
+    authorize @paramedic_type
+  end
+
+  def update
+    @paramedic_type = ParamedicType.find(params[:id])
+    authorize @paramedic_type
+    @paramedic_type.update(paramedic_type_params)
+  end
+
   def destroy
+  end
+
+  private
+
+  def paramedic_type_params
+    params.require(:paramedic_type).permit(:title, :acronym)
   end
 end
