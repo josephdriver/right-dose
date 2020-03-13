50d34692a479e85e3bd016cee1101a3cba4a0e47
Joseph Driver <joseph.driver0@gmail.com>
2020-03-13 17:03:31 +0800

    commented controller

--- app/controllers/case_drugs_controller.rb
+++ app/controllers/case_drugs_controller.rb
@@ -12,13 +12,13 @@ class CaseDrugsController < ApplicationController
 end
 
 
-  drugs = Drug.all
-  available_drugs = drugs.select do |drug|
-      drug.presentations.each do |drug_pres|
-        drug_pres.indications.each do |drug_ind|
-          drug_ind.rules.each do |drug_rul|
-            drug_rul.paramedic_type == current_paramedic
-          end
-        end
-      end
-    end
+  # drugs = Drug.all
+  # available_drugs = drugs.select do |drug|
+  #     drug.presentations.each do |drug_pres|
+  #       drug_pres.indications.each do |drug_ind|
+  #         drug_ind.rules.each do |drug_rul|
+  #           drug_rul.paramedic_type == current_paramedic
+  #         end
+  #       end
+  #     end
+  #   end
