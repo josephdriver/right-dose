d0377cd9ebe398a687e19f4c4792313f3b078099
Joseph Driver <joseph.driver0@gmail.com>
2020-03-13 19:16:53 +0800

    skdjfsk

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -15,16 +15,15 @@ class DashboardsController < ApplicationController
       @drug = Drug.find(params[:drug][:drug_id])
       @paramedic_type = current_paramedic.paramedic_type
       @age = 7
-      @pediatric_cutoff = 12
       if params[:drug]
         rules = @drug.rules.where(paramedic_type: @paramedic_type)
         rules_presentation = rules.select { |rule| rule.indication.presentation == Presentation.find(params[:drug][:presentation_ids]) }
         rules_indication = rules_presentation.select { |rule| rule.indication == Indication.find(params[:drug][:indication_ids]) }
         rules_route = rules_indication.select { |rule| rule.route == Route.find(params[:other][:routes]) }
 
-        if @age > @pediatric_cutoff
+        if @age > @paramedic.organization.pediatric_cutoff
           @final_rule = rules_route.select { |rule| rule.patient_type == "Adult" }
-        elsif @age <= @pediatric_cutoff
+        elsif @age <= @paramedic.organization.pediatric_cutoff
           @final_rule = rules_route.select { |rule| rule.patient_type == "Pediatric"}
           @final_rule = @final_rule.select do |rule|
             rule.min_age <= @age && rule.max_age >= @age
