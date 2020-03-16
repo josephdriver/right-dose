ae5e0f55cdcfe861f06964e49ac9e927ab25411b
Yassin Elsir <yassinmelsir@gmail.com>
2020-03-16 12:42:53 +0800

    added add scope form

--- app/controllers/paramedic_types_controller.rb
+++ app/controllers/paramedic_types_controller.rb
@@ -1,13 +1,25 @@
 class ParamedicTypesController < ApplicationController
   before_action :authenticate_admin!
-  def new
-  end
 
   def index
     @paramedic_types = policy_scope(ParamedicType)
   end
 
+  def new
+    @paramedic_type = ParamedicType.new(paramedic_type_params)
+    authorize @paramedic_type
+  end
+
   def create
+    @paramedic_type = ParamedicType.new(paramedic_type_params)
+    @paramedic_type.organization = current_admin.organization
+    authorize @paramedic_type
+    if @paramedic_type.save
+      redirect_to paramedic_types_path
+    else
+      flash[:alert] = 'scope not saved'
+      redirect_to paramedic_types_path
+    end
   end
 
   def edit
